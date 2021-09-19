1af120c1312d03dff5b9dfc62251e9bfaa1359e1
Yilfri <yilfri97@hotmail.com>
2021-09-19 13:03:54 -0500

    Price ready

---
+++ src/components/PriceCard.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+	margin-right: 50px;
+	padding: 30px;
+	box-shadow: rgba(0, 0, 0, 0.35) 0px 4px 20px 0px;
+	background-color: white;
+	border-radius: 10px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+const PriceContainer = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+const Price = styled.span`
+	font-weight: bold;
+	font-size: 50px;
+`;
+
+const Type = styled.button`
+	padding: 10px;
+	margin: 10px 0;
+	border: 1.5px solid crimson;
+	color: crimson;
+	background-color: white;
+	border-radius: 20px;
+`;
+
+const List = styled.ul`
+	list-style: none;
+`;
+
+const ListItem = styled.li`
+	margin: 30px 0;
+`;
+
+const Button = styled.button`
+	border: none;
+	background-color: darkblue;
+	color: white;
+	font-weight: bold;
+	font-size: 16px;
+	padding: 15px;
+	border-radius: 10px;
+	cursor: pointer;
+`;
+
+const PriceCard = ({ price, type }) => {
+	return (
+		<Container>
+			<PriceContainer>
+				$<Price>{price}</Price>/month
+			</PriceContainer>
+			<Type>{type} Plan</Type>
+			<List>
+				<ListItem>200 Hand-Crafted Templates</ListItem>
+				<ListItem>Exclusive Support</ListItem>
+				<ListItem>50+ PreBuildt Websites</ListItem>
+				<ListItem>Premium Plugins</ListItem>{' '}
+			</List>
+			<Button>Join Now</Button>
+		</Container>
+	);
+};
+
+export default PriceCard;
